4fa62aff354ae99fb7bb885712aef15256e5b7b9
Bruno Staine <brunostaine@hotmail.com>
2024-01-01 18:59:29 -0300

    Config api e service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { LoginComponent } from './modules/pages/login/login.component';
 import { RegisterComponent } from './modules/pages/register/register.component';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,13 +19,15 @@ import { RegisterComponent } from './modules/pages/register/register.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
+    HttpClientModule,
+    ToastModule,
 
     // COMPONENTS STANDALONE
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
 
-  providers: [CookieService],
+  providers: [CookieService, MessageService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
