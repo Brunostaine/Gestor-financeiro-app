70bc5c6f7abaac152dc4b0693d0cf5f7afe688d3
Bruno Staine <brunostaine@hotmail.com>
2024-01-03 17:05:09 -0300

    buscando as finanças

--- src/app/modules/pages/dashboard/dashboard.component.ts
+++ src/app/modules/pages/dashboard/dashboard.component.ts
@@ -1,18 +1,52 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { Subject, takeUntil } from 'rxjs';
+import { FinancaService } from 'src/app/core/services/financa/financa.service';
+import { FinancaTableComponent } from '../../components/financa-table/financa-table.component';
+import { GetAllFinancasResponse } from './../../../shared/models/interfaces/financas/response/getAllFinancasResponse';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
   standalone: true,
-  imports: [CommonModule, DashboardComponent]
+  imports: [CommonModule, DashboardComponent, FinancaTableComponent],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  private ref!: DynamicDialogRef;
+  public financasData: any;
 
-  constructor() { }
+  constructor(
+    private financaService: FinancaService,
+    private dialogService: DialogService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.buscarFinancasAPI();
   }
 
+  buscarFinancasAPI() {
+    this.financaService
+      .getAllfinancas()
+
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+
+          this.financasData = response;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
